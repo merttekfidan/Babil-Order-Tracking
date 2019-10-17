8bd3b3b8f8dc0ba13894e60a132ec2a012c02273
Merdjan Tekfidan <merttekfidan@gmail.com>
2019-10-17 20:07:26 +0300

    No changes

--- public/assets/pages/morris.init.js
+++ public/assets/pages/morris.init.js
@@ -198,7 +198,7 @@
     $.MorrisCharts = new MorrisCharts, $.MorrisCharts.Constructor = MorrisCharts
 }(window.jQuery),
 
-//initializing 
+//initializing
 function($) {
     "use strict";
     $.MorrisCharts.init();
