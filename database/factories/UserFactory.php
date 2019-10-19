8403a45c39f5d7bdc71215c727003cb4990d1fc0
Merdjan Tekfidan <merttekfidan@gmail.com>
2019-10-19 21:59:50 +0300

    Create default user in factory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -18,10 +18,10 @@ use Illuminate\Support\Str;
 
 $factory->define(User::class, function (Faker $faker) {
     return [
-        'name' => $faker->name,
-        'email' => $faker->unique()->safeEmail,
+        'name' => 'Merdjan Tekfidan',
+        'email' => 'merttekfidan@gmail.com',
         'email_verified_at' => now(),
-        'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+        'password' => '$2y$10$xzAkpQYAhicmqlsPLw58TOn.8p6fj9Lfizt3fYK3k4MVdzKuwQnPO', // password
         'remember_token' => Str::random(10),
     ];
 });
