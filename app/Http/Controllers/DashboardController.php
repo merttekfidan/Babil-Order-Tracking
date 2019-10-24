ad6ce77478b3bbd50cfe933a08068cd5b2ebc238
Merdjan Tekfidan <merttekfidan@gmail.com>
2019-10-24 20:45:38 +0300

    Implement commercial statistics

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Order;
+use App\Commercial;
 use Carbon\Carbon;
 
 class DashboardController extends Controller
@@ -21,12 +22,10 @@ class DashboardController extends Controller
     public function index(Order $orders)
     {
         $todays_orders = $orders->daily_orders();
-
         $orders_this_month = $orders->get_orders_custom();
         $orders_last_month = $orders->get_orders_custom($this->today());
         $orders_return_count = $orders_this_month->where('statuses_id', 5)->count();
         $orders_cancel_count = $orders_this_month->where('statuses_id', 3)->count();
-
         $orders_this_month_till_today = $orders->get_orders_custom($this->firstOfMonth(), $this->today())->count();
         $orders_last_month_till_today = $orders->get_orders_custom($this->firstOfMonth()->subMonth(), $this->today()->subMonth())->count();
 
@@ -52,7 +51,15 @@ class DashboardController extends Controller
         $products_this_month_till_today_cancel = $this->get_products_count($orders->get_monthly_products($this->firstOfMonth(), $this->today(), 3));
         $products_last_month_till_today_cancel = $this->get_products_count($orders->get_monthly_products($this->firstOfMonth()->subMonth(), $this->today()->subMonth(), 3));
 
+        $commercials = Commercial::all();
+        $commercials_this_month = $commercials->whereBetween('date', [$this->firstOfMonth(), $this->today()])->sum('price');
+        $commercials_last_month = $commercials->whereBetween('date', [$this->firstOfMonth()->subMonth(), $this->today()->subMonth()])->sum('price');
 
+        $orders_havale_this_month = $orders->get_orders_custom($this->firstOfMonth(), $this->today())->where('payment_methods_id', 3)->count();
+        $orders_havale_last_month = $orders->get_orders_custom($this->firstOfMonth()->subMonth(), $this->today()->subMonth())->where('payment_methods_id', 3)->count();
+
+        $products_this_month_havale = $this->get_products_count($orders->get_monthly_products($this->firstOfMonth(), $this->today())->where('payment_methods_id', 3));
+        $products_last_month_havale = $this->get_products_count($orders->get_monthly_products($this->firstOfMonth()->subMonth(), $this->today()->subMonth())->where('payment_methods_id', 3));
 
         return view('dashboard.index')->with('todays_orders', $todays_orders)
                                       ->with('orders_return_count', $orders_return_count)
@@ -72,7 +79,13 @@ class DashboardController extends Controller
                                       ->with('products_this_month_till_today_return', $products_this_month_till_today_return)
                                       ->with('products_last_month_till_today_return', $products_last_month_till_today_return)
                                       ->with('products_this_month_till_today_cancel', $products_this_month_till_today_cancel)
-                                      ->with('products_last_month_till_today_cancel', $products_last_month_till_today_cancel);
+                                      ->with('products_last_month_till_today_cancel', $products_last_month_till_today_cancel)
+                                      ->with('commercials_this_month', $commercials_this_month)
+                                      ->with('commercials_last_month', $commercials_last_month)
+                                      ->with('orders_havale_this_month', $orders_havale_this_month)
+                                      ->with('orders_havale_last_month', $orders_havale_last_month)
+                                      ->with('products_this_month_havale', $products_this_month_havale)
+                                      ->with('products_last_month_havale', $products_last_month_havale);
     }
     public function get_products_count($products)
     {
