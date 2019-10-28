54b66f6c8b539080e71d491def3c24e6b11bbadb
Merdjan Tekfidan <merttekfidan@gmail.com>
2019-10-28 04:03:48 +0300

    Implement model files

--- app/City.php
+++ app/City.php
@@ -7,8 +7,4 @@ use Illuminate\Database\Eloquent\Model;
 class City extends Model
 {
     public $timestamps = false;
-    public function orders()
-    {
-        return $this->hasMany('App\Order');
-    }
 }
