a5161f70d177f94d957b8ee1a4e541cf7c9052ff
Merdjan Tekfidan <merttekfidan@gmail.com>
2019-10-29 23:43:21 +0300

    Change settings of seeds

--- database/seeds/RoleUserSeeder.php
+++ database/seeds/RoleUserSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\RoleUser;
 
 class RoleUserSeeder extends Seeder
 {
@@ -11,6 +12,19 @@ class RoleUserSeeder extends Seeder
      */
     public function run()
     {
-        factory('App\RoleUser', 1)->create();
+        DB::table('role_user')->delete();
+        $users = [
+        [
+          'role_id'=>1,
+          'user_id'=>1
+        ],
+        [
+          'role_id'=>2,
+          'user_id'=>2
+        ]
+         ];
+        foreach ($users as $user) {
+            RoleUser::create($user);
+        }
     }
 }
--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
+use App\User;
 
 class UserSeeder extends Seeder
 {
@@ -12,6 +14,36 @@ class UserSeeder extends Seeder
     public function run()
     {
         DB::table('users')->delete();
-        factory('App\User', 1)->create();
+        $users = [
+          [
+            'name' => 'Merdjan',
+            'surname' => 'Tekfidan',
+            'nickname' => 'mertcaner',
+            'phone_number' => '5364161577',
+            'email' => 'merttekfidan@gmail.com',
+            'email_verified_at' => now(),
+            'password' => bcrypt('112233+-+-'), // password
+            'remember_token' => Str::random(10),
+            'last_online' => date('Y-m-d H:i:s'),
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s'),
+          ],
+          [
+              'name' => 'Merdjan2',
+              'surname' => 'Tekfidan',
+              'nickname' => 'mertcaner2',
+              'phone_number' => '5364161577',
+              'email' => 'merttekfidan2@gmail.com',
+              'email_verified_at' => now(),
+              'password' => bcrypt('112233+-+-'), // password
+              'remember_token' => Str::random(10),
+              'last_online' => date('Y-m-d H:i:s'),
+              'created_at' => date('Y-m-d H:i:s'),
+              'updated_at' => date('Y-m-d H:i:s'),
+          ]
+           ];
+        foreach ($users as $user) {
+            User::create($user);
+        }
     }
 }
