974400e99ed42cd0e85459508dd5a2805c5246bb
Merdjan Tekfidan <merttekfidan@gmail.com>
2019-10-29 23:43:09 +0300

    Add relationship

--- app/Role.php
+++ app/Role.php
@@ -7,4 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class Role extends Model
 {
     public $timestamps = false;
+    public function users()
+    {
+        return $this->belongsToMany('App\User', 'role_user');
+    }
 }
