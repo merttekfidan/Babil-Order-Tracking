43930a80299ea9672e6ba0bff3cb24f89c4d1163
Merdjan Tekfidan <merttekfidan@gmail.com>
2020-08-10 14:06:21 +0200

    Add user seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -16,5 +16,6 @@ class DatabaseSeeder extends Seeder
         $this->call(StatusSeeder::class);
         $this->call(OrderSeeder::class);
         $this->call(CommercialSeeder::class);
+        $this->call(UserSeeder::class);
     }
 }
